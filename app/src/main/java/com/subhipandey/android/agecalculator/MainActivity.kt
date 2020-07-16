2a311c0c10511e43c6ed477202cf6f4eb154f07c
Subhi Pandey <subhi2012cpi@gmail.com>
2020-07-16 10:58:15 +0530

    finalized ui

--- app/src/main/java/com/subhipandey/android/agecalculator/MainActivity.kt
+++ app/src/main/java/com/subhipandey/android/agecalculator/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.subhipandey.android.agecalculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.content_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnDatePicker.setOnClickListener {view ->
+            clickDatePicker(view)
+
+        }
+
+
+
+    }
+
+    fun clickDatePicker(view: View){
+
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{
+                view,year,month, dayOfMonth ->
+
+        }
+            ,year
+            ,month,
+            day).show()
     }
 }
